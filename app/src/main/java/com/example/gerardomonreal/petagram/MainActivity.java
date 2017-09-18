3a62f83469d725aae87c83a9dce9a564ddcf5e57
GerardoMonrealJimenez <gerardo.monreal.imt@gmail.com>
2017-09-17 19:32:34 -0500

    commit
    first commit

---
+++ app/src/main/java/com/example/gerardomonreal/petagram/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.gerardomonreal.petagram;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import adapter.PageAdapter;
+import java.util.ArrayList;
+
+import fragments.MascotasFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    private TabLayout tabLayout;
+    ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar     = (Toolbar) findViewById(R.id.toolBar);
+        viewPager   = (ViewPager) findViewById(R.id.viwePager);
+        tabLayout   =   (TabLayout) findViewById(R.id.tabLayout);
+        setSupportActionBar(toolbar);
+        setUpViewPager();
+    }
+
+    private ArrayList<Fragment> agregandoFragments()
+    {
+        ArrayList<Fragment> fragments = new ArrayList<>();
+        fragments.add(new MascotasFragment());
+
+        return fragments;
+    }
+
+    public void setUpViewPager()
+    {
+
+        viewPager.setAdapter(new PageAdapter(getSupportFragmentManager(),agregandoFragments()));
+        tabLayout.setupWithViewPager(viewPager);
+        try {
+            tabLayout.getTabAt(0).setIcon(R.drawable.ic_sentiment_very_satisfied_white_18dp);
+        }catch (Exception ex)
+        {
+
+
+
+
+            throw ex;
+        }
+
+
+
+
+
+    }
+}
